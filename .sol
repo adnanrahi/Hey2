0ae2ebcfa5cc0404f1a8f50a92de917a527d0482
adnanrahi <71041943+adnanrahi@users.noreply.github.com>
2022-05-07 19:21:32 +0530

    Create .sol

---
+++ .sol
@@ -0,0 +1,9 @@
+hello sir 
+i am from fiverr 
+can you connect with telegram or whatsapp for better conversation 
+
+telegram- @Petersmith18 
+
+whatsapp- +91 9525572767
+
+kindly donot discuss about it in fiverr
